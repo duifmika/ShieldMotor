f3be3220f4179b7bdf9bc722d1bd6fe7d4ed8aee
duifmika <vlagerig_knuffels_3l@icloud.com>
2025-03-21 14:26:23 +0100

    Update MotionControl.cpp
    made correction

--- src/MotionControl.cpp
+++ src/MotionControl.cpp
@@ -1,6 +1,36 @@
 #include "MotionControl.h"
 #include <math.h>
 
+void MotionControl::goForward(){
+    m_frontLeft-> run(FORWARD);
+    m_frontRight-> run(FORWARD);
+    m_backLeft-> run(FORWARD);
+    m_backRight-> run(FORWARD);
+}
+void MotionControl::goLeft(){
+    m_frontLeft-> run(BACKWARD);
+    m_frontRight-> run(FORWARD);
+    m_backLeft-> run(FORWARD);
+    m_backRight-> run(BACKWARD);   
+}
+void MotionControl::goBackward(){
+    m_frontLeft-> run(BACKWARD);
+    m_frontRight-> run(BACKWARD);
+    m_backLeft-> run(BACKWARD);
+    m_backRight-> run(BACKWARD); 
+}
+void MotionControl::goRight(){
+    m_frontLeft-> run(FORWARD);
+    m_frontRight-> run(BACKWARD);
+    m_backLeft-> run(BACKWARD);
+    m_backRight-> run(FORWARD); 
+}
+void MotionControl::goBrake(){
+    m_frontLeft-> run(RELEASE);
+    m_frontRight-> run(RELEASE);
+    m_backLeft-> run(RELEASE);
+    m_backRight-> run(RELEASE);
+}
 void MotionControl::init(double cellWidth, double wallWidth, 
             ShieldMotor* frontLeft, ShieldMotor* frontRight, 
             ShieldMotor* backLeft, ShieldMotor* backRight) {
@@ -32,6 +62,16 @@ double MotionControl::calculateDistance(double fromX, double fromY, int8_t toX, 
     return sqrt(deltaX * deltaX + deltaY * deltaY) * (m_cellWidth + m_wallWidth);
 }
 
+CompassDirMC MotionControl::radiansToDirection(double angleRad) const {
+    angleRad = fmod(angleRad, 2 * PI);
+    if (angleRad < 0) angleRad += 2*PI;
+
+    if (angleRad >= 7.0 / 4.0 * PI || angleRad < 1.0 / 4.0 * PI) return CompassDirMC::North;
+    if (angleRad >= 1.0 / 4.0 * PI && angleRad < 3.0 / 4.0 * PI) return CompassDirMC::East;
+    if (angleRad >= 3.0 / 4.0 * PI && angleRad < 5.0 / 4.0 * PI) return CompassDirMC::South;
+    return CompassDirMC::West;
+}
+
 void MotionControl::drive(double fromX, double fromY, int8_t toX, int8_t toY) {
     // update m_carRotation and m_heading
     double distCm = calculateDistance(fromX, fromY, toX, toY);
@@ -41,7 +81,8 @@ void MotionControl::drive(double fromX, double fromY, int8_t toX, int8_t toY) {
         // if dist is very low (the car has arrived) stop the car if in motion
         // Adjust the 3 cm dist if it doesnt work
         if (distCm < 3 && m_frontLeft->getSpeed() > 0) {
-            // Stop the car
+            goBrake();
+            delay(100); // remtijd bepalen
         }
 
         // rotate 90 degrees to the right heading 
@@ -50,8 +91,46 @@ void MotionControl::drive(double fromX, double fromY, int8_t toX, int8_t toY) {
         return;
     }
 
-    // correction
+    if (m_frontLeft->getSpeed() == 0) {
+      double offX = fromX - floor(fromX);
+      double offY = fromY - floor(fromY);
+      if (radiansToDirection(m_carRotation) == CompassDirMC::North){
+        if (offX >= 0.05){
+          goLeft();
+        }
+        else if(offX <= -0.05){
+          goRight();
+        }
 
-    // go forward
+      }
+      if (radiansToDirection(m_carRotation) == CompassDirMC::South){
+        if (fabs(offX) >= 0.05){
+          goRight();
+        }
+        else if(fabs(offX) <= -0.05){
+          goLeft();
+        }
+      }
+      if (radiansToDirection(m_carRotation) == CompassDirMC::East){
+        if (fabs(offY) >= 0.05){
+          goLeft();
+        }
+        else if(fabs(offY) <= -0.05){
+          goRight();
+        }
+      }
+      if (radiansToDirection(m_carRotation) == CompassDirMC::West){
+        if (fabs(offY) >= 0.05){
+          goRight();
+        }
+        else if(fabs(offY) <= -0.05){
+          goLeft();
+        }
+      }
+
+      goBrake();
+    }
+
+    goForward();
 
 }
